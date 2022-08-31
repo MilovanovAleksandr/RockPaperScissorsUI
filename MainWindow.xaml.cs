416f33f53af43718af43104d168dc130f56fa582
aleksandr.milovanov.985@gmail.com <aleksandr.milovanov.985@gmail.com>
2022-08-31 20:55:48 +0300

    Add method "GuessingFigure", add event click button "rockBtn".

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,5 +21,66 @@ namespace RockPaperScissorsUI {
         public MainWindow() {
             InitializeComponent();
         }
+
+        private int victory = 0;
+        private int defeat = 0;
+        private int draw = 0;
+
+        private int numberFigure;
+        private string figure;
+
+        private void GuessingFigure() {
+            rockCompLbl.Visibility = Visibility.Hidden;
+            scissorsCompLbl.Visibility = Visibility.Hidden;
+            paperCompLbl.Visibility = Visibility.Hidden;
+            Random rand = new Random(DateTime.Now.Millisecond);
+            numberFigure = rand.Next(0, 3);
+            switch (numberFigure) {
+                case 0:
+                figure = "Камень";
+                break;
+
+                case 1:
+                figure = "Ножницы";
+                break;
+
+                case 2:
+                figure = "Бумага";
+                break;
+
+                default:
+                break;
+            }
+        }
+
+        private void rockBtn_Click(object sender, RoutedEventArgs e) {
+            rockUserLbl.Visibility = Visibility.Visible;
+            GuessingFigure();
+            switch(figure) {
+                case "Камень":
+                rockCompLbl.Visibility = Visibility.Visible;
+                resultTextBlock.Text = "Результат раунда: НИЧЬЯ";
+                draw++;
+                drawLbl.Content = $"Кол-во ничьих: {draw}";
+                break;
+
+                case "Ножницы":
+                scissorsCompLbl.Visibility = Visibility.Visible;
+                resultTextBlock.Text = "Результат раунда: ВЫ ПОБЕДИЛИ";
+                victory++;
+                victoryUserLbl.Content = $"Кол-во ваших побед: {victory}";
+                break;
+
+                case "Бумага":
+                paperCompLbl.Visibility = Visibility.Visible;
+                resultTextBlock.Text = "Результат раунда: КОМПЬЮТЕР ПОБЕДИЛ";
+                defeat++;
+                victoryCompLbl.Content = $"Кол-во побед компьютера: {defeat}";
+                break;
+
+                default:
+                break;
+            }
+        }
     }
 }
